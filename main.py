4a44a177230da08f62b868d224dd322401ce5e98
Abdallah said <abdallahelkssas104@gmail.com>
2026-03-26 20:39:15 +0200

    Implement book checkout, return book and fine calculation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from database import DatabaseManager
 from book import Book
 from person import Member, Librarian
+import time
 
 
 def main():
@@ -8,26 +9,35 @@ def main():
     db.add_dummy_data()
     db.close()
 
-    print("Library system test")
+    print("\nLibrary System Test")
 
-    print("Test Librarian")
+    print("\nLibrarian Action")
     librarian = Librarian("Admin", "ID-EMP-001", "admin@getzew.com")
-    print(librarian)
+    librarian.add_book_item("B0020", "9780132350884", "D5")
 
-    librarian.add_book_item("B0011", "9780132350884", "D1")
-
-    print("\nTest Member")
+    print("\nMember Checkout")
     member = Member.get_member("ID-MEM-001")
-    if member:
-        print(member)
-        print(f"Can checkout? {member.can_checkout()}")
 
-    print("\nBook Search")
-    search_result = Book.search_books("Clean Code", "title")
-    for result in search_result:
-        print(result)
+    if member:
+        print(f"Member: {member.name} | Books Checked out: {member.checkout_count}")
+
+        member.checkout_book("B0007")
+
+        member.checkout_book("B9999")
+
+        print("\nCurrently Borrowed Books")
+        borrowed = Member.get_borrowed_books(member.id)
+        for record in borrowed:
+            print(f"Title: {record[0]}, Barcode: {record[1]}, Due: {record[2]}")
+
+        print("\nReturn Book")
+        member.return_book("B0007")
+
+        print("\n--- Borrowed Books After Return ---")
+        borrowed = Member.get_borrowed_books(member.id)
+        if not borrowed:
+            print("No books currently borrowed.")
 
 
 if __name__ == '__main__':
     main()
-
--- person.py
+++ person.py
@@ -1,4 +1,5 @@
 from database import DatabaseManager
+from datetime import datetime, timedelta
 
 
 class Person:
@@ -18,8 +19,9 @@ Email:{self.email}"""
 class Member(Person):
     MaxBooksLimit = 5
     MaxDaysLimit = 10
+    FinePerDay = 5
 
-    def __init__(self, name, person_id, email, checkout_count = 0):
+    def __init__(self, name, person_id, email, checkout_count=0):
         super().__init__(name, person_id, email)
         self.checkout_count = checkout_count
 
@@ -36,6 +38,94 @@ class Member(Person):
             return Member(member[0][0], member[0][1], member[0][2], member[0][3])
         return None
 
+    def checkout_book(self, barcode):
+        if not self.can_checkout():
+            print(f"ERROR: {self.name} has reached the maximum number of books({self.MaxBooksLimit} books)")
+            return False
+
+        db = DatabaseManager()
+        query = "SELECT * FROM book_items WHERE barcode = ? AND status = 'available'"
+        item = db.fetch_query(query, (barcode,))
+
+        if not item:
+            print(f"ERROR: book item {barcode} is not available")
+            db.close()
+            return False
+
+        query = "UPDATE book_items SET status = 'loaned' WHERE barcode = ?"
+        db.execute_query(query, (barcode,))
+
+        today = datetime.now().date()
+        due_date = today + timedelta(days=self.MaxDaysLimit)
+
+        query = "INSERT INTO loans (member_id, book_barcode, issue_date, due_date) VALUES (? , ?, ?, ?)"
+        db.execute_query(query, (self.id, barcode, str(today), str(due_date)))
+
+        query = "UPDATE members SET checkout_count = checkout_count + 1 WHERE member_id = ?"
+        db.execute_query(query, (self.id,))
+        self.checkout_count += 1
+
+        db.close()
+        print(f"Success: book {barcode} checked out to {self.name} due to {due_date}")
+
+        return True
+
+    def return_book(self, barcode):
+        db = DatabaseManager()
+
+        query = "SELECT loan_id, due_date FROM loans WHERE book_barcode = ? AND return_date IS NULL"
+        loan = db.fetch_query(query, (barcode,))
+
+        if not loan:
+            print(f"ERROR: No active loan found for book {barcode}")
+            db.close()
+            return False
+
+        loan_id = loan[0][0]
+        due_date = datetime.strptime(loan[0][1], "%Y-%m-%d").date()
+        today = datetime.now().date()
+        fine = self.calculate_fine(today, due_date)
+        self.update_db_on_return(db, barcode, loan_id, today, fine)
+
+        return True
+
+    def update_db_on_return(self, db: DatabaseManager, barcode, loan_id, today, fine):
+        query = "UPDATE loans SET return_date = ?, fine_amount = ? WHERE loan_id = ?"
+        db.execute_query(query, (str(today), fine, loan_id))
+
+        query = "UPDATE book_items SET status = 'available' WHERE barcode = ?"
+        db.execute_query(query, (barcode,))
+
+        query = "UPDATE members SET checkout_count = checkout_count - 1 WHERE member_id = ?"
+        db.execute_query(query, (self.id,))
+        self.checkout_count -= 1
+
+        db.close()
+        print(f"Success: Book {barcode} returned.")
+
+    def calculate_fine(self, today, due_date):
+        fine = 0
+        if today > due_date:
+            days_late = (today-due_date).days
+            fine = days_late * self.FinePerDay
+            print(f"Book is {days_late} days late : Fine ----> {fine} Egyptian bound")
+        else:
+            print(f"Book is returned on time, No Fine")
+
+        return fine
+
+    @staticmethod
+    def get_borrowed_books(member_id):
+        db = DatabaseManager()
+        query = """
+            SELECT b.title, l.book_barcode, l.due_date FROM loans l
+            JOIN book_items bi ON l.book_barcode = bi.barcode 
+            JOIN books b ON bi.isbn = b.isbn WHERE l.member_id = ? AND l.return_date IS NULL
+        """
+        borrowed_books = db.fetch_query(query, (member_id,))
+        db.close()
+        return borrowed_books
+
 
 class Librarian(Person):
     def __init__(self, name, employee_id, email):
@@ -44,7 +134,7 @@ class Librarian(Person):
     @staticmethod
     def add_book_item(barcode, isbn, rack):
         db = DatabaseManager()
-        query = "SELECT * FROM books WHERE isbn = ?"
+        query = "SELECT * FROM book_items WHERE barcode = ?"
 
         if db.fetch_query(query, (barcode,)):
             print(f"Error: Barcode {barcode} already exist")
@@ -60,4 +150,5 @@ class Librarian(Person):
         db.execute_query(query, (barcode, isbn, rack))
         db.close()
         print(f"New book item {barcode} added to rack {rack}.")
+
 
